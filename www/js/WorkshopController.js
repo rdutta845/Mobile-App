d7212c8c2f6161de8a4db72aa3e8fcdfd7661ad8
Rahul Dutta <rdutta845@gmail.com>
2017-06-29 11:12:49 +0530

    worshop page done

--- www/js/WorkshopController.js
+++ www/js/WorkshopController.js
@@ -1,6 +1,10 @@
 angular.module('starter.controllers')
 .controller('WorkshopController', function(CONFIG, $scope, $stateParams, $location, $http) {
 
+
+    $scope.scheduleWorkshop = [];
+    $scope.pastWorkshop = [];
+    var curDate = new Date();
     $http({
       method:"GET",
       url:CONFIG.apiEndpoint+"/getallworkshopsinfo",
@@ -9,8 +13,19 @@ angular.module('starter.controllers')
         if(response.data.result != null){
             console.log("RESPONSE Workshop", response.data.result);
             $scope.userWorkshop = response.data.result;
-          
           }
+          console.log("current Date", curDate);
+          $scope.userWorkshop.forEach(function(data, id){
+            var comDate = new Date(data.date);
+            console.log("comDate", comDate);
+            if(curDate > comDate){
+              $scope.pastWorkshop.push(data);
+            }else if(curDate <= comDate){
+              $scope.scheduleWorkshop.push(data);
+            }
+
+          })
+
         })
     $scope.register = function () {
       console.log("move on");
