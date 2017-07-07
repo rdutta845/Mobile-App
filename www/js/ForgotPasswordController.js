889dd50d025b0bf9b1adeb4d78c31ffd444b9ac7
Dinesh Raja <dineshrajaa.93@gmail.com>
2017-07-07 23:51:54 +0530

    Removed Sidemenu from Forgot Password page

--- www/js/ForgotPasswordController.js
+++ www/js/ForgotPasswordController.js
@@ -1,40 +1,44 @@
 angular.module('starter.controllers')
-.controller('ForgotPasswordController', function(CONFIG, $scope, $location, $window, $http, $rootScope, $auth, $ionicPopup) {
-
+	.controller('ForgotPasswordController', function (CONFIG, $scope, $ionicSideMenuDelegate, $location, $window, $http, $rootScope, $auth, $ionicPopup) {
+		$ionicSideMenuDelegate.canDragContent(false);
+		$scope.$on('$ionicView.leave', function () {
+			// Enable swipe to open menu while leaving this page
+			$ionicSideMenuDelegate.canDragContent(true)
+		});
 		$scope.email = {
-			value:''
+			value: ''
 		}
-		$scope.chngPass = function(){
-			if($scope.email.value == null || $scope.email.value == ''){
+		$scope.chngPass = function () {
+			if ($scope.email.value == null || $scope.email.value == '') {
 				var alertPopup = $ionicPopup.alert({
-		      title: 'Error',
-		      template: 'please enter your mail id'
-		    });
-		    alertPopup.then(function(res) {
+					title: 'Error',
+					template: 'please enter your mail id'
+				});
+				alertPopup.then(function (res) {
 					console.log("please enter your mail id")
-		    });
-			}else{
+				});
+			} else {
 				$http({
-					method : "POST",
-					url : CONFIG.apiEndpoint+'/sendchangepassmail/',
-					data: {handle : $scope.email.value}
+					method: "POST",
+					url: CONFIG.apiEndpoint + '/sendchangepassmail/',
+					data: { handle: $scope.email.value }
 				}).then(function mySucces(response) {
-					if(response.data.error){
+					if (response.data.error) {
 						var alertPopup = $ionicPopup.alert({
-				      title: 'Error',
-				      template: response.data.message
-				    });
-				    alertPopup.then(function(res) {
+							title: 'Error',
+							template: response.data.message
+						});
+						alertPopup.then(function (res) {
 							console.log(response.data.message)
-				    });
-					}else{
+						});
+					} else {
 						var alertPopup = $ionicPopup.alert({
-				      title: 'Success',
-				      template: "change password link successfully sent to you mail id"
-				    });
-				    alertPopup.then(function(res) {
+							title: 'Success',
+							template: "change password link successfully sent to you mail id"
+						});
+						alertPopup.then(function (res) {
 							console.log("change password link successfully sent to your mail id")
-				    });
+						});
 					}
 					// console.log(response);
 				}, function myError(response) {
@@ -42,6 +46,6 @@ angular.module('starter.controllers')
 				});
 			}
 		}
-		
 
-})
+
+	})
